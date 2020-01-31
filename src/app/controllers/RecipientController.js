45c9153fb49b62830211940fb9ba8df8a3f36b7f
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-01-31 00:22:31 -0300

    criançao do model, route e controllers de recipient

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,32 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+    async index(req, res) {
+        const recipients = Recipient.findAll();
+
+        return res.json(recipients);
+    }
+
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            street: Yup.string().required(),
+            number: Yup.string().required(),
+            complement: Yup.string(),
+            state: Yup.string().required(),
+            city: Yup.string().required(),
+            postalcode: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: 'Validation fails' });
+        }
+
+        const recipient = await Recipient.create(req.body);
+
+        return res.json(recipient);
+    }
+}
+
+export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import AuthMiddleware from './app/middlewares/Auth';
 
@@ -13,5 +14,8 @@ routes.use(AuthMiddleware);
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
